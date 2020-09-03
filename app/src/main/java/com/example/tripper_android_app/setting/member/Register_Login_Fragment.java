a1a50a8497dec6332f550d3fb897e443c5db688b
zhiiitin <>
2020-09-03 16:58:39 +0800

    LoginFragment ＆ MemberFragment  Update

--- app/src/main/java/com/example/tripper_android_app/setting/member/Register_Login_Fragment.java
+++ app/src/main/java/com/example/tripper_android_app/setting/member/Register_Login_Fragment.java
@@ -1,4 +1,4 @@
-package com.example.tripper_android_app.setting;
+package com.example.tripper_android_app.setting.member;
 
 import android.os.Bundle;
 
@@ -7,38 +7,28 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tripper_android_app.R;
 
 
-public class Register_main_Fragment extends Fragment {
+public class Register_Login_Fragment extends Fragment {
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setHasOptionsMenu(true);
-    }
 
-    private void setHasOptionsMenu() {
-    }
-
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-
-        inflater.inflate(R.menu.app_bar_button,menu);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_register_main_, container, false);
+        return inflater.inflate(R.layout.fragment_register__login_, container, false);
     }
 
     @Override
---
+++ app/src/main/java/com/example/tripper_android_app/setting/member/Register_Member_Fragment.java
@@ -0,0 +1,35 @@
+package com.example.tripper_android_app.setting.member;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tripper_android_app.R;
+
+
+public class Register_Member_Fragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_register__member_, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
