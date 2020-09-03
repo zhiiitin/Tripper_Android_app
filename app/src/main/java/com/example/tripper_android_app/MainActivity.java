f8cdfe2dc6acc6aa81af28b97ccb52378c1a0a38
zhiiitin <>
2020-09-03 15:18:09 +0800

    Update Setting_Register Fragment  and Update styles.xml

--- app/src/main/java/com/example/tripper_android_app/MainActivity.java
+++ app/src/main/java/com/example/tripper_android_app/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tripper_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/com/example/tripper_android_app/trip/Main_Trip_Fragment.java
+++ app/src/main/java/com/example/tripper_android_app/trip/Main_Trip_Fragment.java
@@ -29,10 +29,12 @@ public class Main_Trip_Fragment extends Fragment {
 
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_main_trip_, container, false);
+
     }
 }
