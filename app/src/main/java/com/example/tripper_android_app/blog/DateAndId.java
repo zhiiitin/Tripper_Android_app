d0f98f202ba2a4a312fedd75cfa57651231b3384
Cooper <>
2020-10-08 16:52:19 +0800

    no message

--- app/src/main/java/com/example/tripper_android_app/blog/DateAndId.java
+++ app/src/main/java/com/example/tripper_android_app/blog/DateAndId.java
@@ -12,13 +12,9 @@ public class DateAndId {
     }
 
 
-<<<<<<< HEAD
-    public void DateAndId(String trip_Id) {
 
-=======
     public DateAndId(String trip_Id) {
         super();
->>>>>>> 0bf104e4554f03e2ac370963ea913fb728fa82e5
         this.trip_Id = trip_Id;
     }
 
