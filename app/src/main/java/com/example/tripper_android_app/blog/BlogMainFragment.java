9b410d1e503606c95243f9fac06e24931291e013
zhiiitin <>
2020-10-14 15:51:01 +0800

    Debug

--- app/src/main/java/com/example/tripper_android_app/blog/BlogMainFragment.java
+++ app/src/main/java/com/example/tripper_android_app/blog/BlogMainFragment.java
@@ -262,10 +262,7 @@ public class BlogMainFragment extends Fragment {
         }
     }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 0548612df02974a081832286ab0b600b12f16e84
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
