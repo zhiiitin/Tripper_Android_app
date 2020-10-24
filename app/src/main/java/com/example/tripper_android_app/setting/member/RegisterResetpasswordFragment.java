969e17e46ee27682f0398671466bbc3cad409e8d
zhiiitin <>
2020-10-24 21:26:40 +0800

    no message

--- app/src/main/java/com/example/tripper_android_app/setting/member/RegisterResetpasswordFragment.java
+++ app/src/main/java/com/example/tripper_android_app/setting/member/RegisterResetpasswordFragment.java
@@ -1,64 +1,119 @@
-package com.example.tripper_android_app;
+package com.example.tripper_android_app.setting.member;
 
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegisterResetpasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.tripper_android_app.MainActivity;
+import com.example.tripper_android_app.R;
+import com.example.tripper_android_app.util.Common;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class RegisterResetpasswordFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private final static String TAG = "TAG_ResetPwFragment";
+    private MainActivity activity;
+    private TextInputEditText etEmailReset ;
+    private ImageButton ibResetPassword ;
+    private FirebaseAuth firebaseAuth;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RegisterResetpasswordFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterResetpasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegisterResetpasswordFragment newInstance(String param1, String param2) {
-        RegisterResetpasswordFragment fragment = new RegisterResetpasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        activity = (MainActivity) getActivity();
+        setHasOptionsMenu(true);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_register_resetpassword, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//ToolBar
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setTitle("忘記密碼");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorForWhite));
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Drawable upArrow = ContextCompat.getDrawable(activity, R.drawable.abc_ic_ab_back_material);
+        if (upArrow != null) {
+            upArrow.setColorFilter(ContextCompat.getColor(activity, R.color.colorForWhite), PorterDuff.Mode.SRC_ATOP);
+            activity.getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        }
+//NavigationButton
+        BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottomBar);
+        NavController navController = Navigation.findNavController(activity, R.id.nav_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        Menu itemMenu = bottomNavigationView.getMenu();
+        itemMenu.getItem(4).setChecked(true);
+
+        etEmailReset = view.findViewById(R.id.etEmailReset);
+        ibResetPassword = view.findViewById(R.id.ibResetPassword);
+
+        ibResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmailReset.getText().toString().trim();
+                if(email.equals("")){
+                    etEmailReset.setError("請輸入正確信箱格式");
+                }else {
+                    firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Common.showToast(activity,"發送成功，請檢查您的電子郵件");
+                            }else {
+                                String error = task.getException().getMessage();
+                                Common.showToast(activity,error);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String message = "";
+        switch (item.getItemId()) {
+            case android.R.id.home:    //此返回鍵ID是固定的
+                Navigation.findNavController(this.getView()).popBackStack();
+                return true;
+
+        }
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
